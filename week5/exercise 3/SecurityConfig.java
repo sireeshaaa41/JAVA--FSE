6d50a7ddd95987204f3fe5f667d2bd577470344f
sireeshaaa41 <sireeshareddy3027@gmail.com>
2025-07-20 13:08:32 +0530

    Create SecurityConfig.java

---
+++ week5/exercise 3/SecurityConfig.java
@@ -0,0 +1,24 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtTokenFilter jwtTokenFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable() // Optional: disable CSRF for JWT
+            .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+            .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
