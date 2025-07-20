90a170d590ff1ee7cc33b39cb6ce72944f67ee4f
sireeshaaa41 <sireeshareddy3027@gmail.com>
2025-07-20 13:02:45 +0530

    Create UserController.java

---
+++ week5/UserController.java
@@ -0,0 +1,11 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.security.Principal;
+
+@RestController
+public class UserController {
+    @GetMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;  // JSON with user details
+    }
+}
