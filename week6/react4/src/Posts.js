9f74fae09c459883b315142f3f2c9dfbe88a8c9e
sireeshaaa41 <sireeshareddy3027@gmail.com>
2025-07-27 12:37:59 +0530

    Create Posts.js

---
+++ week6/react4/src/Posts.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Figure 3: Posts Component (Conceptual structure before filling methods)
+class Posts extends React.Component {
+  // Constructor, state, and methods will go here
+  render() {
+    return (
+      <div>
+        <h2>Blog Posts</h2>
+        {/* Posts will be rendered here */}
+      </div>
+    );
+  }
+}
+
+export default Posts;
